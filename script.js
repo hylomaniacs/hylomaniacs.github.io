7a07e5fa574952392a55b38cc40c985384dfd33a
hylomaniacs <32606078+hylomaniacs@users.noreply.github.com>
2017-10-07 23:29:39 -0400

    More responsive updates

--- script.js
+++ script.js
@@ -7,21 +7,7 @@ $(document).ready(function() {
     	$("body, html").animate({scrollTop: 0}, 1000);
     });
     
-    var marginHeightSum = 40 + 40 + 100 + $(window).height();
-    var footHeight = 200;
-    
-    var foot = function() {
-        $("#top").css("height", $(window).height());
-   		$("#head").css("top", 30 + $(window).height());
-        $("#wrap").height($(document).height());
-        if(
-            ($("#wrap").height() > $("#main").height()+marginHeightSum+footHeight)
-            && ($(window).height() > $("#main").height()+marginHeightSum+footHeight)
-            && ($("#wrap").height() > $(window).height())
-        )
-        	$("#wrap").height($(window).height());
-        $("#foot").css("top", $("#wrap").height()-footHeight);
-    };
-    foot();
-    $(window).resize(foot);
+    $(window).resize(function() {
+      $("#top").css("height", $(window).height());
+    }).resize();
 });
